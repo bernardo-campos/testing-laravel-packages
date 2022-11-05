637d5da9a9f7795ebdfe4dc8701d29c7eedb940b
Bernardo <camposbg@gmail.com>
2022-11-05 03:15:45 -0300

    model Author

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\AuthorController;
 use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
+use LaravelJsonApi\Laravel\Http\Controllers\JsonApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +13,7 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
+
+JsonApiRoute::server('v1')->prefix('v1')->resources(function ($server) {
+    $server->resource('authors', JsonApiController::class)->readOnly();
+});
